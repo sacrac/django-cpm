c1eb3777c35c0f29e0f08b11ea617ce514949331
goldhand <will@webpowerlabs.com>
2013-08-13 21:38:24 -0700

    Project wizard cat list can specify parents with jquery.sortable

--- cpm/cpm/urls.py
+++ cpm/cpm/urls.py
@@ -22,6 +22,5 @@ urlpatterns = patterns('',
                        url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
                        url(r'^accounts/', include('accounts.urls', namespace='accounts')),
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^snippets/', 'core.views.snippet_list', name='snippet-list')
-                       url(r'^$', ProjectRedirectView.as_view()),
+                       url(r'^$', ProjectRedirectView.as_view())
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
